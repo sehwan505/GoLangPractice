6cfee37f2eeca8eb584e8f05797cf06e9069e23c
sehwan505 <sehwan505@gmail.com>
2024-12-23 21:29:31 +0900

    Feat: pickNumber change to go style

--- pickNumber/main.go
+++ pickNumber/main.go
@@ -5,17 +5,28 @@ import (
 	"math/rand"
 )
 
+func inputValue() (int, error) {
+	var a int
+	fmt.Printf("I've picked a number between 1 and 100. Can you guess it?\n")
+	_, err := fmt.Scanln(&a)
+	if err != nil {
+		return 0, err
+	}
+	return a, nil
+}
+
 func main() {
 	for {
 		n := rand.Intn(100) + 1
-		var a int
-		fmt.Printf("I've picked a number between 1 and 100. Can you guess it?\n")
-		fmt.Scanf("%d", &a)
-
-		if a == n {
-			fmt.Printf("Congratulations! You've guessed the number!\n")
+		input, err := inputValue()
+		if err != nil {
+			fmt.Print("Invalid input. Please enter a number.\n")
+			return
+		}
+		if input == n {
+			fmt.Printf("You've guessed it right!\n")
 		} else {
-			fmt.Printf("Sorry, the number was %d\n", n)
+			fmt.Printf("You've guessed it wrong. The number was %d\n", n)
 		}
 	}
 }
